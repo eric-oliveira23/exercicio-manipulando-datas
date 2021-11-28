7af702b1c04fadca7c62ac7d9dc2201873cd9d3f
Eric <ericpoliveira@gmail.com>
2021-11-28 17:59:15 -0300

    manipulando-datas

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manipulandodatas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime data;
+
+            
+
+            Console.WriteLine("A primeira parcela irá ser ");
+            data = DateTime.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("A segunda parcela irá ser: "+data.AddMonths(1));
+           
+
+            Console.WriteLine("A terceira parcela irá ser: "+data.AddMonths(2));
+            
+
+            Console.WriteLine("A quarta parcela irá ser: "+ data.AddMonths(3));
+            
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
